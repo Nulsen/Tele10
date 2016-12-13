92cc585d561ce724954dae0b8e97307be3a2f53f
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2016-12-13 11:23:36 +0100

    minor updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
     function configureWaypoints() {
-        var waypoint = new Waypoint({
+        new Waypoint({
             element: document.body,
             offset: 0,
             handler: function(direction) {
                 $('#hero .caption, #hero .discover').addClass('triggered');
+            }
+        });
+
+        new Waypoint({
+            element: document.getElementById('customers'),
+            offset: '80%',
+            handler: function(direction) {
+                $('#customers .title').addClass('triggered');
+            }
+        });
+
+        new Waypoint({
+            element: document.getElementById('customers'),
+            offset: '70%',
+            handler: function(direction) {
+                $('#customers .img-container').addClass('triggered');
             }
         });
     }
