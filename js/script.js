35d5067938ba2c1096c8d999579f504cbec9f4d9
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2017-05-09 20:29:29 +0200

    Product and services

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-    $('#read-more').click((e) => {
+    $('#read-more').click(function(e) {
         e.preventDefault();
 
         $('html, body').stop().animate({
             scrollTop: $('.pitch').offset().top
         }, 500);
+    });
+
+    $('.clients .logos img').click(function() {
+        if ($(this).hasClass('active') || window.innerWidth < 768) {
+            return;
+        }
+
+        $('.clients .logos .active').removeClass('active');
+        $(this).parent().addClass('active');
+
+        const index = parseInt($(this).data('quote'), 10);
+
+        $('.clients .quotes .active').stop().fadeOut(function() {
+            $('.clients .quotes .active').removeClass('active');
+            $('.clients .quote').eq(index - 1).addClass('active').stop().fadeIn();
+        });
     });
     // function is_touch_device() {
     //     return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
