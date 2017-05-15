aca88a3d8f0d956982ddbf7abdec711739ccb289
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2017-05-15 13:38:16 +0200

    Updated script

--- js/script.js
+++ js/script.js
@@ -39,10 +39,12 @@ $(document).ready(function() {
 
     function postForm($form) {
         $.post($form[0].action, $form.serialize()).then(function(response) {
-            grecaptcha.reset();
+            $form[0].reset();
             $form.find('.success').text(response).slideDown();
         }).fail(function(err) {
             $form.find('.error').text(err.responseText).slideDown();
+        }).done(function() {
+            grecaptcha.reset();
         });
     }
 });
