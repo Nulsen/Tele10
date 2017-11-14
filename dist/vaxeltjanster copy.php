30b8afbb616d44dc397d2a13eee8831e9b6b80a6
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2017-11-14 17:20:37 +0100

    Fixes

---
+++ dist/vaxeltjanster copy.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML><html><head> <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+<meta name="description" content="Molnbaserad v&auml;xel, fast eller mobil.">
+<link rel="icon" href="favicon.ico">
+<meta property="og:title" content="Tele10 V&auml;xeltj&auml;nst">
+<meta property="og:type" content="article">
+<meta property="og:url" content="http://www.tele10.se/">
+<meta property="og:description" content="Molnbaserad v&auml;xel, fast eller mobil.">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Tele10 - Växeltjänster</title> </head><body> <script async>
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-98888977-1', 'auto');
+ga('send', 'pageview');
+</script> <nav class="nav">
+    <div class="container">
+        <ul>
+            <li class="brand">
+                <a href="/">
+                    <img src="img/logo.png" alt="Tele10">
+                </a>
+            </li>
+            <li class="item">
+                <a href="/om-tele10">Om Tele10</a>
+            </li>
+            <li class="item active">
+                <a href="/vaxeltjanster">Växeltjänster</a>
+            </li>
+            <li class="item">
+                <a href="/anvandartjanster">Användartjänster</a>
+            </li>
+            <li class="item">
+                <a href="/anvandartjanster#mobile">Mobiltelefoni</a>
+            </li>
+            <li class="item dropdown">
+                <span>Support</span>
+                <ul>
+                    <li class="item">
+                        <a href="/faq">FAQ</a>
+                    </li>
+                    <li class="item">
+                        <a href="/driftinformation">Driftinformation</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="item">
+                <a href="/kontakt">Kontakt</a>
+            </li>
+            <li class="item right dropdown">
+                <span>Logga in</span>
+                <ul>
+                    <li class="item">
+                        <a href="https://hem.tele10.se/">Hem.tele10.se</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="button"></div>
+    </div>
+</nav> <div class="hero products-and-services"><div class="caption"><h1>Växeltjänster</h1></div></div><div class="preamble"><div class="container"><div class="row no-gutters"><div class="message col-12"><h2 class="title">En v&auml;xel f&ouml;r alla!</h2><p><strong>Tele10 molnväxel</strong> &auml;r en kostnadseffektiv och funktionell IP-baserad v&auml;xeltj&auml;nst f&ouml;r att via internet koppla samman flera kontor, distansarbetsplatser eller medarbetare som reser mycket.</p><p>V&auml;xeltj&auml;nsten &auml;r anpassad f&ouml;r alla typer av f&ouml;retag.</p></div></div></div></div><div class="products grey multiple"><div class="container"><div class="row no-gutters"><div class="col-12 col-md-6 product"><div class="row no-gutters text-center"><div class="col-12"><img src="img/product-mobil.png" alt="Tele10 Mobil"></div><div class="col-12 text-container"><h2 class="title">Tele10 Mobil</h2><h5 class="sub-title">V&auml;lj den typ av mobiltelefon som passar dig.</h5><p class="info">Ladda ned appen <strong>Tele10</strong> så f&aring;r du alla v&auml;xelns funktioner direkt i mobilen.</p></div></div></div><div class="col-12 col-md-6 product"><div class="row no-gutters text-center"><div class="col-12"><img src="img/Gigaset-SL610-PRO-phone.png" alt="Tele10 Fast"></div><div class="col-12 text-container"><h2 class="title">Tele10 Fast</h2><h5 class="sub-title">V&auml;lj den typ av fasttelefon som passar dig.</h5><p class="info">Fast &mdash; avancerad, enklare eller b&auml;rbar.</p></div></div></div></div></div></div><div class="products multiple"><div class="container"><div class="row no-gutters"><div class="col-12 col-md-6 product"><div class="row no-gutters text-center"><div class="col-12"><img src="img/product-soft.png" alt="Tele10 Soft"></div><div class="col-12 text-container"><h2 class="title">Tele10 Soft</h2><h5 class="sub-title">V&auml;lj softphone som passar dig.</h5><p class="info">Fungerar &auml;ven med videosamtal.</p></div></div></div><div class="col-12 col-md-6 product"><div class="row no-gutters text-center"><div class="col-12"><img src="img/showcase.png" alt="Tele10 Mix"></div><div class="col-12 text-container"><h2 class="title">Tele10 Mix</h2><h5 class="sub-title">Anv&auml;nd b&aring;de fasttelefon, softphone eller mobil.</h5><p class="info">Du kan ha samma anknytningsnummer och direktval p&aring; alla enheter.</p></div></div></div></div></div></div> <div class="copyright small col-12">
+    Copyright © <script>document.write(new Date().getFullYear());</script> Tele10. All rights reserved.
+</div> <script type="text/javascript" src="script/script.js"></script></body></html>
