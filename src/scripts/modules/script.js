c7558303d2a6f8dd71f82a6e1a230d1308c1b34b
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2019-11-14 12:10:37 +0100

    new structure

--- src/scripts/modules/script.js
+++ src/scripts/modules/script.js
@@ -1,87 +1,98 @@
 $(document).ready(function() {
-    $('#read-more').click(function(e) {
-        e.preventDefault();
+	$('#read-more').click(function(e) {
+		e.preventDefault();
 
-        $('html, body')
-            .stop()
-            .animate(
-                {
-                    scrollTop: $('.pitch').offset().top - 59,
-                },
-                500
-            );
-    });
+		$('html, body')
+			.stop()
+			.animate(
+				{
+					scrollTop: $('.pitch').offset().top - 59,
+				},
+				500
+			);
+	});
 
-    $('.nav .button').click(function(e) {
-        e.preventDefault();
+	$('.nav .button').click(function(e) {
+		e.preventDefault();
 
-        $('.nav ul').toggleClass('active');
-    });
+		$('.nav ul').toggleClass('active');
+	});
 
-    if (window.location.pathname == '/tele10-molnvaxel') {
-        $('.nav [href="/tele10-molnvaxel#mobile"]').click(function(e) {
-            e.preventDefault();
+	if (window.location.pathname == '/tele10-molnvaxel') {
+		$('.nav [href="/tele10-molnvaxel#mobile"]').click(function(e) {
+			e.preventDefault();
 
-            $('.nav ul').removeClass('active');
+			$('.nav ul').removeClass('active');
 
-            $('html, body')
-                .stop()
-                .animate(
-                    {
-                        scrollTop: $('#mobile').offset().top - 59,
-                    },
-                    500
-                );
-        });
-    }
+			$('html, body')
+				.stop()
+				.animate(
+					{
+						scrollTop: $('#mobile').offset().top - 59,
+					},
+					500
+				);
+		});
+	}
 
-    $('.contact form').submit(function(e) {
-        e.preventDefault();
+	$('.contact form').submit(function(e) {
+		e.preventDefault();
 
-        const $message = $(e.target).find('.success:visible, .error:visible');
+		const $message = $(e.target).find('.success:visible, .error:visible');
 
-        $(this)
-            .find('button')
-            .addClass('disabled');
+		$(this)
+			.find('button')
+			.addClass('disabled');
 
-        if ($message.length) {
-            $message
-                .slideUp()
-                .promise()
-                .then(function() {
-                    postForm($(e.target));
-                });
-        } else {
-            postForm($(e.target));
-        }
-    });
+		if ($message.length) {
+			$message
+				.slideUp()
+				.promise()
+				.then(function() {
+					postForm($(e.target));
+				});
+		} else {
+			postForm($(e.target));
+		}
+	});
 
-    function postForm($form) {
-        $.post($form[0].action, $form.serialize())
-            .then(function(response) {
-                if ('ga' in window) {
-                    var tracker = ga.getAll()[0];
-                    if (tracker) {
-                        tracker.send('event', 'Contact form', 'Send');
-                    }
-                }
+	function postForm($form) {
+		$.post($form[0].action, $form.serialize())
+			.then(function(response) {
+				if ('ga' in window) {
+					var tracker = ga.getAll()[0];
+					if (tracker) {
+						tracker.send('event', 'Contact form', 'Send');
+					}
+				}
 
-                $form[0].reset();
-                $form
-                    .find('.success')
-                    .html(response)
-                    .slideDown();
-            })
-            .fail(function(err) {
-                $form
-                    .find('.error')
-                    .html(err.responseText)
-                    .slideDown();
-            })
-            .done(function() {
-                $form.find('button').removeClass('disabled');
-            });
-    }
+				$form[0].reset();
+				$form
+					.find('.success')
+					.html(response)
+					.slideDown();
+			})
+			.fail(function(err) {
+				$form
+					.find('.error')
+					.html(err.responseText)
+					.slideDown();
+			})
+			.done(function() {
+				$form.find('button').removeClass('disabled');
+			});
+	}
 
-    $('.accordion').accordion();
+	for (let i = 0; i < 3; i++) {
+		let waypoint = new Waypoint({
+			element: $('.pitch .box')[i],
+			handler() {
+				this.destroy();
+				$(this.element).addClass('animated fadeIn');
+			},
+			offset: '75%'
+		});
+	}
+
+	$('.accordion').accordion();
 });
